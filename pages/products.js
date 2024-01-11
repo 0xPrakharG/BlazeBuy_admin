92edff3f732156912319430cc84e40258bbe84ca
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-01-12 00:48:17 +0530

    feat: google authentication & navigation bar

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+import Link from "next/link";
+
+export default function Products() {
+  return (
+    <Layout>
+      <Link
+        className="rounded-md bg-blue-900 px-2 py-1 text-white"
+        href={"/products/new"}
+      >
+        Add new peoduct
+      </Link>
+    </Layout>
+  );
+}
